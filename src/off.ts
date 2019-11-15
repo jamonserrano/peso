5da88c4bda947234603a823ce7fbf3dfba00e640
jamonserrano <honti.viktor@gmail.com>
2019-11-15 18:27:14 +0100

    Change argument order in off()

--- src/off.ts
+++ src/off.ts
@@ -1,10 +1,9 @@
 import { Item } from "./types";
 
 export const off = (
-	event: string,
 	listener: (e: Event) => any,
 	options: EventListenerOptions = {}
-) => (item: Item | Window) => {
+) => (event: string) => (item: Item | Window) => {
 	item.removeEventListener(event, listener, options);
 
 	return item;
--- types/off.d.ts
+++ types/off.d.ts
@@ -1,1 +1,1 @@
-export declare const off: (event: string, listener: (e: Event) => any, options?: EventListenerOptions) => (item: Element | Text | Document | DocumentFragment | Window) => Element | Text | Document | DocumentFragment | Window;
+export declare const off: (listener: (e: Event) => any, options?: EventListenerOptions) => (event: string) => (item: Element | Text | Document | DocumentFragment | Window) => Element | Text | Document | DocumentFragment | Window;
