c93ebe1c92c23528f2134961dcb05fd554b6df0b
jamonserrano <honti.viktor@gmail.com>
2019-11-15 18:16:00 +0100

    Normalize after replace

--- src/replaceWith.ts
+++ src/replaceWith.ts
@@ -2,7 +2,9 @@ import { Item } from "./types";
 
 export const replaceWith = (subject: Item | string) => (item: Item) => {
 	if (item instanceof Element || item instanceof Text) {
+		const parentNode = item.parentNode;
 		item.replaceWith(subject);
+		parentNode && parentNode.normalize();
 	}
 
 	return item;
