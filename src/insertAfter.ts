183613f91f85173829877431aabdcc2ad3139426
jamonserrano <honti.viktor@gmail.com>
2019-11-15 18:28:55 +0100

    Simplify insertAfter()

--- src/insertAfter.ts
+++ src/insertAfter.ts
@@ -1,13 +1,10 @@
 import { Item } from "./types";
 
-export const insertAfter = (target: Item) => {
+export const insertAfter = (target: Item) => (item: Item) => {
 	if (target instanceof Element || target instanceof Text) {
-		return (item: Item) => {
-			target.after(item);
-			target.parentNode && target.parentNode.normalize();
-			return item;
-		};
-	} else {
-		return (item: Item) => item;
+		target.after(item);
+		target.parentNode && target.parentNode.normalize();
 	}
+
+	return item;
 };
