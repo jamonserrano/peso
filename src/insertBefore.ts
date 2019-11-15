240155e35f08277b51259cfbc40cd7336b9c45ec
jamonserrano <honti.viktor@gmail.com>
2019-11-15 18:28:47 +0100

    SImplify insertBefore()

--- src/insertBefore.ts
+++ src/insertBefore.ts
@@ -1,13 +1,10 @@
 import { Item } from "./types";
 
-export const insertBefore = (target: Item) => {
+export const insertBefore = (target: Item) => (item: Item) => {
 	if (target instanceof Element || target instanceof Text) {
-		return (item: Item) => {
-			target.before(item);
-			target.parentNode && target.parentNode.normalize();
-			return item;
-		};
-	} else {
-		return (item: Item) => item;
+		target.before(item);
+		target.parentNode && target.parentNode.normalize();
 	}
+
+	return item;
 };
