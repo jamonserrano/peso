932a5e18cae72f24a2261fccf250526a4e4c3d9f
jamonserrano <honti.viktor@gmail.com>
2019-11-15 18:30:26 +0100

    Add line break

--- src/element.ts
+++ src/element.ts
@@ -1,5 +1,6 @@
 export const element = (type: string, props: { [key: string]: string }) => {
 	const el = document.createElement(type);
 	Object.entries(props).forEach(([key, value]) => (el[key] = value));
+
 	return el;
 };
