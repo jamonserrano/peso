ec90a67b95d407904f36f020e47d1e6742f53756
jamonserrano <honti.viktor@gmail.com>
2019-11-20 17:50:37 +0100

    addClass() accepts multiple class names

--- src/addClass.ts
+++ src/addClass.ts
@@ -2,7 +2,7 @@ import { Item } from "./types";
 import { isElement } from "./isElement";
 
 export const addClass = (className: string) => (item: Item) => {
-	isElement(item) && item.classList.add(className);
+	isElement(item) && item.classList.add(...className.split(" "));
 
 	return item;
 };
