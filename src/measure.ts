1d60e405cb2cc7f252f64d616ca0ed21faa1e724
jamonserrano <honti.viktor@gmail.com>
2019-11-21 19:47:14 +0100

    Remove measure()

--- src/measure.ts
+++
@@ -1,4 +0,0 @@
-import { isElement } from "./isElement";
-
-export const measure = (item: Element | Text | Document | DocumentFragment) =>
-	isElement(item) ? item.getBoundingClientRect() : null;
--- types/measure.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const measure: (item: Element | Text | Document | DocumentFragment) => DOMRect | null;
