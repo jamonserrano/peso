35f4cf907e6d6cffc5bc94218f11825bf591c262
jamonserrano <honti.viktor@gmail.com>
2019-11-21 18:52:22 +0100

    Curry on()

--- types/on.d.ts
+++ types/on.d.ts
@@ -1,1 +1,1 @@
-export declare const on: (listener: (e: Event) => any, options?: EventListenerOptions) => (event: string) => (item: Element | Text | Document | DocumentFragment | Window) => Element | Text | Document | DocumentFragment | Window;
+export declare const on: (options?: EventListenerOptions) => (listener: (e: Event) => any) => (event: string) => (item: Element | Text | Document | DocumentFragment | Window) => Element | Text | Document | DocumentFragment | Window;
