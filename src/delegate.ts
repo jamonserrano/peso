b5945374e5d4a6776dfc0cdecbd1d1267f1396cc
jamonserrano <honti.viktor@gmail.com>
2019-11-21 18:51:46 +0100

    Curry delegate()

--- src/delegate.ts
+++ src/delegate.ts
@@ -1,8 +1,8 @@
 import { isElement } from "./isElement";
 
-export const delegate = (listener: (event: Event) => any, selector: string) => (
-	e: Event
-) => {
+export const delegate = (listener: (event: Event) => any) => (
+	selector: string
+) => (e: Event) => {
 	const target = e.target;
 	isElement(target) && target.matches(selector) && listener.call(target, e);
 };
--- types/delegate.d.ts
+++ types/delegate.d.ts
@@ -1,1 +1,1 @@
-export declare const delegate: (listener: (event: Event) => any, selector: string) => (e: Event) => void;
+export declare const delegate: (listener: (event: Event) => any) => (selector: string) => (e: Event) => void;
