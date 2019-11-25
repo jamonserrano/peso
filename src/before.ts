d6dde09581dd55e28433aae02e5846a9e8cb72b2
jamonserrano <honti.viktor@gmail.com>
2019-11-25 11:48:09 +0100

    before() strict arguments

--- src/before.ts
+++ src/before.ts
@@ -1,13 +1,8 @@
-import { isElement } from "./isElement";
-import { isText } from "./isText";
-
-export const before = (
-	subject: Element | Text | Document | DocumentFragment | string
-) => (item: Element | Text | Document | DocumentFragment) => {
-	if (isElement(item) || isText(item)) {
-		item.before(subject);
-		item.parentNode && item.parentNode.normalize();
-	}
+export const before = (subject: Element | Text | DocumentFragment | string) => (
+	item: Element | Text
+) => {
+	item.before(subject);
+	item.parentNode && item.parentNode.normalize();
 
 	return item;
 };
