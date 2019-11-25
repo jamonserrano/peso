305b99a01c7b5f25cd0bad2f96fa9f4312a720e1
jamonserrano <honti.viktor@gmail.com>
2019-11-25 11:53:35 +0100

    delegate() strict types

--- src/delegate.ts
+++ src/delegate.ts
@@ -1,8 +1,6 @@
-import { isElement } from "./isElement";
-
 export const delegate = (listener: (event: Event) => any) => (
 	selector: string
 ) => (e: Event) => {
-	const target = e.target;
-	isElement(target) && target.matches(selector) && listener.call(target, e);
+	const target = e.target as Element;
+	target.matches(selector) && listener.call(target, e);
 };
