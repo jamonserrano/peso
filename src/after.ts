7e24c726f595d67ecb5725846ae2c1f5f098c51e
jamonserrano <honti.viktor@gmail.com>
2019-11-25 11:43:29 +0100

    after() strict arguments

--- src/after.ts
+++ src/after.ts
@@ -1,13 +1,8 @@
-import { isElement } from "./isElement";
-import { isText } from "./isText";
-
-export const after = (
-	subject: Element | Text | Document | DocumentFragment | string
-) => (item: Element | Text | Document | DocumentFragment) => {
-	if (isElement(item) || isText(item)) {
-		item.after(subject);
-		item.parentNode && item.parentNode.normalize();
-	}
+export const after = (subject: Element | Text | DocumentFragment | string) => (
+	item: Element | Text
+) => {
+	item.after(subject);
+	item.parentNode && item.parentNode.normalize();
 
 	return item;
 };
